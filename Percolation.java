07da95c628896c6bec8286136bcd3137e6abf968
mtripathi093@gmail.com <mtripathi093@gmail.com>
2018-12-17 22:28:59 -0800

    New commit

---
+++ Percolation.java
@@ -0,0 +1,105 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+/**
+ * Implementation of the percolation data structure.
+ * @author Manas
+ */
+public class Percolation {
+
+    private final WeightedQuickUnionUF quickUnionStructure;
+    private final WeightedQuickUnionUF quickUnionStructureForIsFull;
+
+    private int gridSize;
+    private final boolean[][] gridstatus;  // to check if the site is open.
+
+    private int virtualTopSite;
+    private int virtualBottomSite;
+    private int openSites;
+
+
+    public Percolation(int n) { // create N-by-N grid, with all the sites blocked
+        if (n <= 0) {
+            throw new IllegalArgumentException("The size of the grid must be at least 1");
+        }
+        gridSize = n;
+        gridstatus = new boolean[n][n];
+
+        quickUnionStructure = new WeightedQuickUnionUF(n * n + 2);  // elements are 0 to N + 1.
+        quickUnionStructureForIsFull = new WeightedQuickUnionUF(n * n + 1); // elememts are 0 to N.
+
+        virtualTopSite = 0; // index for the virtal top side in quickUnionStructure.
+        virtualBottomSite = n * n + 1; //index for virtual bottom side in the quickUnionStructure.
+    }
+
+    public void open(int row, int col) {    // open site (row , column ) if it is not open already
+        int fieldIndex = getSingleDimensionIndex(row, col);
+
+
+        if (!isOpen(row, col)) {
+            if (row == 1) { // if it is the first row, irrespective of the column.
+                quickUnionStructure.union(virtualTopSite, fieldIndex);
+                quickUnionStructureForIsFull.union(virtualTopSite, fieldIndex);
+            }
+            if (row == gridSize) { // if it is the last row, irresepective of whatever column it is.
+                quickUnionStructure.union(virtualBottomSite, fieldIndex);
+            }
+
+            connectIfOpen(fieldIndex, row + 1, col);
+            connectIfOpen(fieldIndex, row - 1, col);
+            connectIfOpen(fieldIndex, row, col - 1);
+            connectIfOpen(fieldIndex, row, col + 1);
+
+            gridstatus[row - 1][col - 1] = true;
+            openSites++;
+
+        }
+    }
+
+    public boolean isOpen(int row, int col) {   // is site (row i, column j) open?
+        return gridstatus[row - 1][col - 1];
+    }
+
+    public boolean isFull(int row, int col) {   // is site (row i, column j) connected to top?
+        if (isOpen(row, col)) { // first, is it open?
+            int fieldIndex = getSingleDimensionIndex(row, col);
+            return quickUnionStructureForIsFull.connected(virtualTopSite, fieldIndex);
+        }
+        return false;
+    }
+
+    public boolean percolates() {   // does the system percolate?
+        return quickUnionStructure.connected(virtualTopSite, virtualBottomSite);
+    }
+
+    public int numberOfOpenSites() {   // number of open sites?
+        return openSites;
+    }
+
+    private void connectIfOpen(int fieldIndex, int i, int j) {
+        try {
+            if (isOpen(i, j)) // if adjacent sites are open, connect current site to them. also connect the current site with adjacent site in the quickUnionStructureForIsFull.
+            {
+                int neighbourFieldIndex = getSingleDimensionIndex(i, j);
+                quickUnionStructure.union(neighbourFieldIndex, fieldIndex);
+                quickUnionStructureForIsFull.union(neighbourFieldIndex, fieldIndex);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            // Ignore the adjancent rows and cols that are outside the grid.
+        }
+    }
+
+    private int getSingleDimensionIndex(int row, int col) {
+        checkIfAcceptableRowCol(row, col);
+        return (row - 1) * gridSize + col;
+    }
+
+    private void checkIfAcceptableRowCol(int row, int col)
+    {
+        if(row < 1 || col < 1)
+        {
+            throw new IllegalArgumentException("Row or Column cannot be less than 1");
+        }
+    }
+
+}
---
+++ PercolationStats.java
@@ -0,0 +1,79 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+
+/**
+ * Perform Monte Carlo Simulation (or Percolation experiemnts) and capture the statistics.
+ * @author Manas
+ */
+public class PercolationStats {
+
+    private final double mean;
+    private final double stddev;
+    private final double confidenceLo;
+    private final double confidenceHi;
+
+
+    public PercolationStats(int N, int T) { // perform T independent experiments on an N-by-N grid
+        if (N <= 0) {
+            throw new IllegalArgumentException("The grid size must be bigger than zero");
+        }
+        if (T <= 0) {
+            throw new IllegalArgumentException("The number of experiments must be bigger than zero");
+        }
+
+        double[] percolationThresholds = new double[T]; // store the value of thresholds for each experiment.
+
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = new Percolation(N);
+
+            while (!percolation.percolates()) {
+                int column;
+                int row;
+
+                do {
+                    column = 1 + StdRandom.uniform(N);
+                    row = 1 + StdRandom.uniform(N);
+                } while (percolation.isOpen(row, column)); // if the column and row generated are open, keep generating new col and row.
+
+                percolation.open(row, column);
+            }
+
+            percolationThresholds[i] = percolation.numberOfOpenSites() / (double) (N * N);
+        }
+
+        mean = StdStats.mean(percolationThresholds);
+        stddev = StdStats.stddev(percolationThresholds);
+        double confidenceFraction = (1.96 * stddev()) / Math.sqrt(T);
+        confidenceLo = mean - confidenceFraction;
+        confidenceHi = mean + confidenceFraction;
+    }
+
+    public double mean() {  // sample mean of percolation threshold
+        return mean;
+    }
+
+    public double stddev() {    // sample standard deviation of percolation threshold
+        return stddev;
+    }
+
+    public double confidenceLo() {  // low  endpoint of 95% confidence interval
+        return confidenceLo;
+    }
+
+    public double confidenceHi() {   // high endpoint of 95% confidence interval
+        return confidenceHi;
+    }
+
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+
+        PercolationStats stats = new PercolationStats(N, T);
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
+    }
+
+}
